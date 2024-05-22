6a095c74ca46be2ea2482511db83e884ee507e9a
Jimako <janamicek@gmail.com>
2024-05-22 22:33:58 +0200

    #4 3.5.8  database functionality

--- includes/mysqli_functions.php
+++ includes/mysqli_functions.php
@@ -4,26 +4,54 @@
 if(!function_exists("dbconnect")) { // just in case.  Some people seemed to be having an issue and this was the easiest fix.
 
 function dbconnect($dbhost, $dbuser, $dbpass, $dbname ) {
-	$mysql_access = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-	if(mysqli_connect_error()) {
-		include(_BASEDIR."languages/en.php"); // Because we haven't got a language set yet.
-		die(_FATALERROR." "._NOTCONNECTED);
+	$mysqli_access = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+ 
+	try
+	{
+		$mysqli_access = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 	}
-	return $mysql_access;
+	catch (mysqli_sql_exception $e)
+	{
+		if (mysqli_connect_error())
+		{
+			include(_BASEDIR . "languages/en.php"); // Because we haven't got a language set yet.
+			die(_FATALERROR . " " . _NOTCONNECTED);
+		}
+	}
+	//mysqli_query($mysql_access, "SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION'");
+	mysqli_query($mysqli_access, "SET SESSION sql_mode = 'NO_ENGINE_SUBSTITUTION'");
+    mysqli_query($mysqli_access, "SET NAMES UTF8;");
+	return $mysqli_access;
 }
 
 
 function dbquery($query) {
 	global $debug, $headerSent, $dbconnect;
 	if($debug && $headerSent) echo "<!-- $query -->\n";
-	$result = $dbconnect->query($query) or accessDenied( _FATALERROR.(isADMIN ? "Query: ".$query."<br />Error: (".$dbconnect->errno.") ".$dbconnect->error : ""));
+	$result = NULL;
+	try
+	{
+			$result = $dbconnect->query($query);
+	}
+	catch (mysqli_sql_exception $e)
+	{
+		if(function_exists('accessDenied')) {
+			accessDenied(_FATALERROR . (isADMIN ? "Query: " . $query . "<br />Error: (" . $dbconnect->errno . ") " . $dbconnect->error : ""));
+		}
+		else {		
+			// this is very rare case. Uncomment this if you really lost what is doing. This is before pagesetup.php, so Admin is not set
+			//	echo "Query: " . $query . "<br />Error: (" . $dbconnect->errno . ") " . $dbconnect->error  <br /> ;
+			die(" <b>A fatal MySQL error was encountered.</b><br /> Check tableprefix. ");	
+		}
+			
+	} 
 	return $result;
 }
 
 function dbnumrows($query) {
 	global $debug, $dbconnect;
 	if ($query === false && $debug) {
-		echo "<!-- dbnumrows ".$dbconnect->mysqli_error." -->\n";
+		echo "<!-- dbnumrows ".$dbconnect->error." -->\n";
 	}
 	return $query->num_rows;
 }
@@ -31,7 +59,7 @@ function dbnumrows($query) {
 function dbassoc($query) {
 	global $debug, $dbconnect;
 	if ($query === false && $debug) {
-		echo "<!-- dbassoc ".$dbconnect->mysqli_error." -->\n";
+		echo "<!-- dbassoc ".$dbconnect->errno." -->\n";
 	}
 	return $query->fetch_assoc();
 }
@@ -44,7 +72,7 @@ function dbinsertid($tablename = 0) {
 function dbrow($query) {
 	global $debug, $dbconnect;
 	if ($query === false && $debug) {
-		if($error) echo "<!-- dbrow ".$dbconnect->mysqli_error." -->\n";
+		if($dbconnect->error) echo "<!-- dbrow ".$dbconnect->errno." -->\n";
 	}
 	return $query->fetch_row();
 }
